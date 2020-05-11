0ea969232d47e8938a7d5c13ca3fc0dee9f100ae
temirlans1 <temirlan.smagulov@nu.edu.kz>
2020-05-12 04:31:01 +0600

    Updates

---
+++ precache-manifest.c0280466fc226e93a6a755d7bd466fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f4f10f3fa547a0b0b1",
+    "url": "/temirlans1/react-face-detection/static/css/main.c9b50ef3.chunk.css"
+  },
+  {
+    "revision": "62f4f10f3fa547a0b0b1",
+    "url": "/temirlans1/react-face-detection/static/js/main.62f4f10f.chunk.js"
+  },
+  {
+    "revision": "0f49efe6b931ebc705a3",
+    "url": "/temirlans1/react-face-detection/static/js/1.0f49efe6.chunk.js"
+  },
+  {
+    "revision": "3ac854c3ae7ff014d6d5",
+    "url": "/temirlans1/react-face-detection/static/js/runtime~main.3ac854c3.js"
+  },
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/temirlans1/react-face-detection/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "73ae8984d3e14e8a921465f70895ce5c",
+    "url": "/temirlans1/react-face-detection/index.html"
+  }
+];
